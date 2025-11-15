f78dd0780fc406a9f910f6f74fe07e1c3ab10a57
Daniel <danielmatt1701@gmail.com>
2025-11-14 22:48:16 -0600

    lista de climas

---
+++ lista.dart
@@ -0,0 +1,34 @@
+import '../clima.dart';
+
+final climas = [
+  Clima(
+      primerEmoji: '🌮',
+      segundoEmoji: '🌧️',
+      titulo: 'Hoy lloverán tacos',
+      recomendacion: 'Lleva servilletas… ¡será una lluvia deliciosa!'
+  ),
+  Clima(
+      primerEmoji: '⚡',
+      segundoEmoji: '🤣',
+      titulo: 'Tormenta de memes',
+      recomendacion: 'Mantén tu teléfono cargado para sobrevivir.'
+  ),
+  Clima(
+      primerEmoji: '☕',
+      segundoEmoji: '☀️',
+      titulo: 'Soleado con olor a café',
+      recomendacion: 'Buen día para programar desde la cama.'
+  ),
+  Clima(
+      primerEmoji: '❄️',
+      segundoEmoji: '😂',
+      titulo: 'Granizada de chistes malos',
+      recomendacion: 'Usa casco, te pueden pegar uno muy malo.'
+  ),
+  Clima(
+      primerEmoji: '💨',
+      segundoEmoji: '😴',
+      titulo: 'Vientos de procrastinación',
+      recomendacion: '¡Cuidado! Podrías posponer todo hasta mañana.'
+  ),
+];
